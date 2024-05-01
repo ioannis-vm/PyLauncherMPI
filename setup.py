f1159d3d7caec752257c7564273e20c6f61810a4
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-05-01 05:33:17 -0700

    Rename directory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyLauncherMPI',
-    version='0.1.0',
+    version='0.1.1',
     author='John Vouvakis Manousakis',
     author_email='ioannis_vm@berkeley.edu',
     description='A simple MPI-based task scheduler for dynamically distributing commands across MPI processes.',
